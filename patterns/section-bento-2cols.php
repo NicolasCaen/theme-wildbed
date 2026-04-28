1b5939085040a54eab917128855f1b7c831733cf
NicolasCaen <pixelea.nicolas@gmail.com>
2026-04-28 09:15:06 +0200

    ancre paiement

--- patterns/section-bento-2cols.php
+++ patterns/section-bento-2cols.php
@@ -9,7 +9,7 @@
 <!-- wp:group {"align":"","className":"section-bento-2cols align-ultrawide","layout":{"type":"constrained"}} -->
 <div class="wp-block-group section-bento-2cols align-ultrawide"><!-- wp:group {"align":"full","className":"section-bento-2cols__row bento-information__row\u002d\u002d1","layout":{"type":"grid","columnCount":2,"minimumColumnWidth":null}} -->
 <div class="wp-block-group alignfull section-bento-2cols__row bento-information__row--1"><!-- wp:group {"className":"is-style-style-1","style":{"spacing":{"padding":{"top":"var:preset|spacing|16-36","bottom":"var:preset|spacing|16-36","left":"var:preset|spacing|16-36","right":"var:preset|spacing|16-36"},"blockGap":"var:preset|spacing|12"},"border":{"radius":{"topLeft":"var:preset|border-radius|s","topRight":"var:preset|border-radius|s","bottomLeft":"var:preset|border-radius|s","bottomRight":"var:preset|border-radius|s"}}},"layout":{"type":"constrained","justifyContent":"left"}} -->
-<div class="wp-block-group is-style-style-1" style="border-top-left-radius:var(--wp--preset--border-radius--s);border-top-right-radius:var(--wp--preset--border-radius--s);border-bottom-left-radius:var(--wp--preset--border-radius--s);border-bottom-right-radius:var(--wp--preset--border-radius--s);padding-top:var(--wp--preset--spacing--16-36);padding-right:var(--wp--preset--spacing--16-36);padding-bottom:var(--wp--preset--spacing--16-36);padding-left:var(--wp--preset--spacing--16-36)"><!-- wp:paragraph -->
+<div id="paiement" class="wp-block-group is-style-style-1" style="border-top-left-radius:var(--wp--preset--border-radius--s);border-top-right-radius:var(--wp--preset--border-radius--s);border-bottom-left-radius:var(--wp--preset--border-radius--s);border-bottom-right-radius:var(--wp--preset--border-radius--s);padding-top:var(--wp--preset--spacing--16-36);padding-right:var(--wp--preset--spacing--16-36);padding-bottom:var(--wp--preset--spacing--16-36);padding-left:var(--wp--preset--spacing--16-36)"><!-- wp:paragraph -->
 <p><strong>Paiements en 4 fois</strong></p>
 <!-- /wp:paragraph -->
 
@@ -50,12 +50,12 @@
 
 <!-- wp:paragraph {"className":"is-label is-style-label"} -->
 <p class="is-label is-style-label">ES</p>
-<!-- /wp:paragraph --></div>
-<!-- /wp:group -->
+<!-- /wp:paragraph -->
 
 <!-- wp:paragraph -->
 <p></p>
 <!-- /wp:paragraph --></div>
+<!-- /wp:group --></div>
 <!-- /wp:group --></div>
 <!-- /wp:group --></div>
 <!-- /wp:group -->
