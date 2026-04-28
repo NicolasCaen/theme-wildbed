1878079a7559835b133b7dd6ec103d52faf8e24b
NicolasCaen <pixelea.nicolas@gmail.com>
2026-04-28 10:00:40 +0200

    ajustement disponibilités

--- patterns/section-disponibilites.php
+++ patterns/section-disponibilites.php
@@ -8,13 +8,12 @@
  */
 ?>
 <?php $is_editor = is_admin() || wp_is_json_request() || (function_exists('get_current_screen') && get_current_screen() && get_current_screen()->is_block_editor()); ?>
-<!-- wp:group {"metadata":{"name":"Disponibilites"},"align":"","className":"align-ultra align-ultrawide","layout":{"type":"constrained","contentSize":"100%"}} -->
+<!-- wp:group {"metadata":{"name":"Disponibilites"},"align":"","className":"align-ultra align-ultrawide","layout":{"type":"default"}} -->
 <div class="wp-block-group align-ultra align-ultrawide"><!-- wp:heading {"align":"full"} -->
 <h2 class="wp-block-heading alignfull">Vérifiez les disponibilités et réservez </h2>
 <!-- /wp:heading -->
 
-<!-- wp:group {"layout":{"type":"constrained"}} -->
-<div id="disponibilites" class="wp-block-group"><!-- wp:shortcode -->
+<!-- wp:html -->
 <?php 
 if (!$is_editor) {
     echo do_shortcode('[ctv_calendar product_id="94258"]');
@@ -22,6 +21,9 @@ if (!$is_editor) {
     echo '[ctv_calendar product_id="94258"]';
 }
 ?>
-<!-- /wp:shortcode --></div>
+<!-- /wp:html -->
+
+<!-- wp:group {"align":"full","layout":{"type":"constrained"}} -->
+<div class="wp-block-group alignfull" id="disponibilites"></div>
 <!-- /wp:group --></div>
 <!-- /wp:group -->
