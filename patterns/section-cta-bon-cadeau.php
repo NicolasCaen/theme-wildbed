8154e6b4434747d0437bbe5399ccdedd559038a5
NicolasCaen <pixelea.nicolas@gmail.com>
2026-05-05 08:49:39 +0200

    maj cta bon cadeau

--- patterns/section-cta-bon-cadeau.php
+++ patterns/section-cta-bon-cadeau.php
@@ -14,11 +14,11 @@
 <!-- /wp:heading -->
 
 <!-- wp:paragraph {"placeholder":"Lorem ipsum dolor sit amet consectetur adipiscing elit sed do eiusmod tempor incididunt ut labore et dolore magna aliqua ut enim ad minim veniam quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur excepteur sint occaecat cupidatat non proident sunt in culpa qui officia deserunt mollit anim id est laborum lorem ipsum dolor sit amet consectetur adipiscing elit sed do eiusmod tempor incididunt ut labore et dolore magna aliqua ut enim ad minim veniam quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur excepteur sint occaecat cupidatat non proident sunt in culpa qui officia deserunt mollit anim id est laborum lorem ipsum dolor sit amet consectetur adipiscing elit sed do eiusmod tempor…"} -->
-<p>Lorem ipsum dolor sit amet consectetur adipiscing elit sed do eiusmod tempor incididunt ut labore et dolore magna aliqua ut enim ad minim veniam quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur excepteur sint occaecat cupidatat non proident sunt in culpa qui officia deserunt mollit anim id est laborum lorem ipsum dolor sit amet consectetur adipiscing .m id est laborum lorem ipsum dolor sit amet consectetur adipiscing elit sed do eiusmod tempor…</p>
+<p>Offrez une expérience inoubliable au cœur de notre camping !Avec ce bon cadeau, laissez vos proches choisir librement parmi toutes nos prestations et vivre un séjour qui leur ressemble.<br>Bon cadeau utilisable pour offrir toutes les prestations du camping (hébergements insolites, emplacement pour tente/caravane/camping-car, repas, animations payantes, locations de vélos...)</p>
 <!-- /wp:paragraph --></div>
 <!-- /wp:group -->
 
 <!-- wp:button {"className":"section-cta-bon-cadeau__btn"} -->
-<div class="wp-block-button section-cta-bon-cadeau__btn"><a class="wp-block-button__link wp-element-button">Offrir un bon cadeau</a></div>
+<div class="wp-block-button section-cta-bon-cadeau__btn"><a class="wp-block-button__link wp-element-button" href="https://bookingpremium.secureholiday.net/FR/16098/giftVoucher?_gl=1*h2rtcj*_ga*MjQwNTIxOTMwLjE3Nzc5MjM3MTE.*_ga_SGSPCSP02V*czE3Nzc5MjM3MTEkbzEkZzEkdDE3Nzc5MjM3NTQkajE3JGwwJGgw&amp;_ga=2.171287648.1128567833.1777923713-240521930.1777923711">Offrir un bon cadeau</a></div>
 <!-- /wp:button --></div>
 <!-- /wp:group -->
