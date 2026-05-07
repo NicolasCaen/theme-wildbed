20e5dcc0a0eefaff540ae8baf814d1dd4909bfd2
NicolasCaen <pixelea.nicolas@gmail.com>
2026-05-07 13:13:36 +0200

    ajout du modele de page texte

---
+++ patterns/modele-page-texte.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Title: Modèle poour les pages de textes
+ * Slug: ng1-base/modele-page-texte
+ * Categories: query
+ * Block Types: core/post-content
+ * Description: Modèle pour les pages de texte
+ *
+ * @package WordPress
+ * @subpackage ng1-base
+ * @since ng1-base 1.0
+ */
+
+?>
+<!-- wp:pattern {"slug":"ng1-base/section-banner-texte--title"} /-->
+  <!-- wp:pattern {"slug":"ng1-base/spacer-32-72"} /-->
---
+++ patterns/section-banner-texte--title.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Title: Section Banner with title
+ * Slug: ng1-base/section-banner-texte--title
+ * Categories: design
+ * Block Types: core/cover
+ */
+?>
+<!-- wp:cover {"overlayColor":"base-2","isUserOverlayColor":true,"align":"full","className":"section-banner-texte is-style-style-2","style":{"spacing":{"padding":{"top":"var:preset|spacing|200","bottom":"var:preset|spacing|56-120"}}}} -->
+<div class="wp-block-cover alignfull section-banner-texte is-style-style-2" style="padding-top:var(--wp--preset--spacing--200);padding-bottom:var(--wp--preset--spacing--56-120)"><span aria-hidden="true" class="wp-block-cover__background has-base-2-background-color has-background-dim-100 has-background-dim"></span><div class="wp-block-cover__inner-container"><!-- wp:group {"metadata":{"name":"Section | Banner avec images"},"align":"wide","className":"section-banner-texte__wrapper is-style-default","style":{"spacing":{"padding":{"top":"var:preset|spacing|10","bottom":"var:preset|spacing|8"}}},"layout":{"type":"constrained","contentSize":"1143px"}} -->
+<div class="wp-block-group alignwide section-banner-texte__wrapper is-style-default" style="padding-top:var(--wp--preset--spacing--10);padding-bottom:var(--wp--preset--spacing--8)"><!-- wp:paragraph {"className":"section-banner-texte__panneau is-style-panneau-rotated"} -->
+<p class="section-banner-texte__panneau is-style-panneau-rotated">Wild Bed  | Les Pâtis</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:post-title {"textAlign":"center","level":1} /-->
+
+<!-- wp:spacer {"height":"var:preset|spacing|7","metadata":{"name":"Espacement 4.5 | 72px","categories":["layout"],"patternName":"ng1-base/spacer-7"},"className":"is-style-spacer-7"} -->
+<div style="height:var(--wp--preset--spacing--7)" aria-hidden="true" class="wp-block-spacer is-style-spacer-7"></div>
+<!-- /wp:spacer --></div>
+<!-- /wp:group --></div></div>
+<!-- /wp:cover -->
