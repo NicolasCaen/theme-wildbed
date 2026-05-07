95bc81a95d799c427df1d94cd9d9719e8a0cb3f7
NicolasCaen <pixelea.nicolas@gmail.com>
2026-05-07 13:36:23 +0200

    banner title

--- patterns/section-banner-texte--title.php
+++ patterns/section-banner-texte--title.php
@@ -8,7 +8,11 @@
 ?>
 <!-- wp:cover {"overlayColor":"base-2","isUserOverlayColor":true,"align":"full","className":"section-banner-texte is-style-style-2","style":{"spacing":{"padding":{"top":"var:preset|spacing|200","bottom":"var:preset|spacing|56-120"}}}} -->
 <div class="wp-block-cover alignfull section-banner-texte is-style-style-2" style="padding-top:var(--wp--preset--spacing--200);padding-bottom:var(--wp--preset--spacing--56-120)"><span aria-hidden="true" class="wp-block-cover__background has-base-2-background-color has-background-dim-100 has-background-dim"></span><div class="wp-block-cover__inner-container"><!-- wp:group {"metadata":{"name":"Section | Banner avec images"},"align":"wide","className":"section-banner-texte__wrapper is-style-default","style":{"spacing":{"padding":{"top":"var:preset|spacing|10","bottom":"var:preset|spacing|8"}}},"layout":{"type":"constrained","contentSize":"1143px"}} -->
-<div class="wp-block-group alignwide section-banner-texte__wrapper is-style-default" style="padding-top:var(--wp--preset--spacing--10);padding-bottom:var(--wp--preset--spacing--8)"><!-- wp:post-title {"textAlign":"center","level":1} /-->
+<div class="wp-block-group alignwide section-banner-texte__wrapper is-style-default" style="padding-top:var(--wp--preset--spacing--10);padding-bottom:var(--wp--preset--spacing--8)"><!-- wp:paragraph {"className":"is-style-panneau-rotated"} -->
+<p class="is-style-panneau-rotated">Wildbed | Les Pâtis</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:post-title {"textAlign":"center","level":1} /-->
 
 <!-- wp:spacer {"height":"var:preset|spacing|7","metadata":{"name":"Espacement 4.5 | 72px","categories":["layout"],"patternName":"ng1-base/spacer-7"},"className":"is-style-spacer-7"} -->
 <div style="height:var(--wp--preset--spacing--7)" aria-hidden="true" class="wp-block-spacer is-style-spacer-7"></div>
